5888a69e5c369cd4cbdf590e293aaa3a5a545081
Gelen Hernandez <gelenhernandezp@gmail.com>
2022-11-03 19:56:37 -0400

    Subiendo Proyecto del portafolio-cv

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Taller de maquetación
 
 repositorio del proyecto
+
+## Proyecto
+
+-[Landing Page de tipo Portafolio-CV](https://GelenMHP.github.com/youtube-git/Portafolio-cv.git)
---
+++ portafolio/script.js
@@ -0,0 +1,56 @@
+/*  MENU */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+  /* esto es para que el menu se ciere con x y el menu y si se da click en alguno sea inicio etc se cierra tambien*/
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* CONTACT FORM FORMULARIO DE CONTACTO */
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/gelenhernandezp@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        $loader.classList.add("none");
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "close";
+        }, 3000); /*3000 segundos se cierra la ventana modal*/
+      });
+  });
+})(document);
